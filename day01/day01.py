3794ba61679a6b8f1d7865ecb9c967b137fb910a
jacksonbelanger <jacksonlbelanger@gmail.com>
2024-12-01 20:06:53 -0500

    part 2

--- day01/day01.py
+++ day01/day01.py
@@ -24,12 +24,18 @@ def part2(list1, list2):
     list2_count = {}
 
     for num in list2:
-        if num in list2:
-            list2[num] += 1
+        if num in list2_count:
+            list2_count[num] += 1
         else:
-            list2[num] = 1
+            list2_count[num] = 1
     
     similarity_score = 0
+
+    for num in list1:
+        if num in list2_count:
+            similarity_score += num * list2_count[num]
+    
+    return similarity_score
 
 if __name__ == "__main__":
     with open("./input.txt", "r") as file:
@@ -38,3 +44,4 @@ if __name__ == "__main__":
     list1, list2 = getLists(lines)
 
     print("Part 1:", part1(list1, list2))
+    print("Part 2:", part2(list1, list2))
