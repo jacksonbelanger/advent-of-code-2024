e7b05e9b8601f95131325c49ffeddacc634bac33
jacksonbelanger <jacksonlbelanger@gmail.com>
2024-12-07 13:49:45 -0500

    day02 part2

--- day02/day02.py
+++ day02/day02.py
@@ -4,54 +4,71 @@ def part1(lines):
     for line in lines:
         nums = [int(num) for num in line.split()]
 
-        safe_reports += is_safe(nums)
+        safe_reports += is_safe(nums, -1)[0]
     
     return safe_reports
 
 def part2(lines):
-    nums = [int(num) for num in line.split()]
+    safe_reports = 0
+    
+    for line in lines:
+        nums = [int(num) for num in line.split()]
 
-    safe_reports += is_really_safe(nums)
+        safe_reports += is_really_safe(nums)
+    
+    return safe_reports
 
-def is_safe(nums):
-    direction = 0
-    unsafe_levels = 0
+def is_safe(nums, indexToSkip):
+    if len(nums) <= 1:
+        return (True, -1)
 
-    for i in range(1, len(nums)):
+    direction = None
+    start = 1 if indexToSkip == 0 else 0
+    prev = nums[start]
+
+    for i in range(start + 1, len(nums)):
+        if indexToSkip == i:
+            continue
+
         cur_direction = 1
-        if nums[i] < nums[i - 1]:
+        if nums[i] < prev:
             cur_direction = -1
         
-        diff = abs(nums[i] - nums[i - 1])
+        diff = abs(nums[i] - prev)
 
-        if (-1 * cur_direction == direction or diff < 1 or diff > 3):
-            return False
+        if ((direction is not None and cur_direction != direction) or diff < 1 or diff > 3):
+            return (False, i)
+        else:
+            direction = cur_direction
 
-        direction = cur_direction
+            # update prev
+            prev = nums[i]
     
-    return True
+    return (True, -1)
 
 def is_really_safe(nums):
-    direction = 0
-    unsafe_levels = 0
+    if is_safe(nums, -1)[0]:
+        return True
 
-    for i in range(1, len(nums)):
-        cur_direction = 1
-        if nums[i] < nums[i - 1]:
-            cur_direction = -1
-        
-        diff = abs(nums[i] - nums[i - 1])
-
-        if (-1 * cur_direction == direction or diff < 1 or diff > 3):
-            return False
-
-        direction = cur_direction
+    for i in range(len(nums)):
+        if is_safe(nums, i)[0]:
+            return True
     
-    return True
+    return False
 
+    # safe = is_safe(nums, -1)
+
+    # if (safe[0]):
+    #     return True
+    
+    # if is_safe(nums, safe[1])[0] or (safe[1] > 0 and is_safe(nums, safe[1] - 1)[0]) or (safe[1] > 1 and is_safe(nums, 0)[0]):
+    #     return True
+
+    # return False    
 
 if __name__ == "__main__":
     with open("./input.txt", "r") as file:
         lines = file.readlines()
     
     print("Part 1", part1(lines))
+    print("Part 2:", part2(lines))
